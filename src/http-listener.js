e638fb6ba94b1456b97e8bf4ac6132b80c3490f3
pedrogongora <pedro.gongora@gmail.com>
2022-09-11 16:25:00 -0500

    feat: cors middleware

--- src/http-listener.js
+++ src/http-listener.js
@@ -1,15 +1,15 @@
 const { httpListener } = require('@marblejs/http')
-const { bodyParser$ } = require("@marblejs/middleware-body")
-const { api$ } = require("./api/api.effects")
+const { bodyParser$ } = require('@marblejs/middleware-body')
+const { cors$ } = require('@marblejs/middleware-cors')
+const { api$ } = require('./api/api.effects')
 const { error$ } = require('./error')
 
 const middlewares = [
+  cors$({ origin: '*', allowHeaders: '*', methods: ['GET', 'POST'] }),
   bodyParser$(),
 ]
 
-const effects = [
-  api$,
-]
+const effects = [api$]
 
 const listener = httpListener({
   middlewares,
